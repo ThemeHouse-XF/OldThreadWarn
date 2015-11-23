83f79b5cceeebc03a10c22428fa6895f3a841294
Jake B <jake.booher@gmail.com>
2015-11-23 10:54:21 -0600

    Initial Commit

---
+++ library/ThemeHouse/OldThreadWarn/Install/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class ThemeHouse_OldThreadWarn_Install_Controller extends ThemeHouse_Install
+{
+
+    protected $_resourceManagerUrl = 'http://xenforo.com/community/resources/old-thread-warning-by-th.3129/';
+
+    protected $_minVersionId = 1020000;
+
+    protected $_minVersionString = '1.2.0';
+}
